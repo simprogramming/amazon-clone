72a688ebe3c154c105358e9b24a3a646bda27c93
Simon <leivaprogramming@gmailcom>
2020-08-21 23:06:01 -0400

    skeleton of amazon-clone app

---
+++ src/Header.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Header.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+const basket = <FontAwesomeIcon icon={faShoppingCart} />
+
+function Header() {
+  return (
+    <nav className="header">
+      <Link to="/">
+        <img className="header__logo" src="/Amazon-logo.png" alt=""/>
+      </Link>
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className='header__nav' >
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Simon</span>
+            <span className="header__optionLineTwo">Sign in</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne" >Returns</span>
+            <span className="header__optionLineTwo" >& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne" >Your</span>
+            <span className="header__optionLineTwo" >Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            {basket}
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Header
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+
+function Home() {
+  return (
+    <div className="home" >
+      <img
+        className="home__image"
+        src="/banner.png"
+        alt=""/>
+    </div>
+  );
+}
+
+export default Home
